abfd36b08fc58fc1fba717de8572cb720e2354b4
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-15 23:51:13 -0600

    handle change

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class NewBoxForm extends Component {
 
+    handleChange() {
+        
+    }
+
     render() {
         <form>
             <label></label>
